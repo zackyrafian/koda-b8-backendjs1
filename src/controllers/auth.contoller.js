b72fe04ff2f3e0d161145ba483a86f18a90cb102
Zacky Rafian Fawwauzy <imftcy@gmail.com>
2026-07-27 15:30:59 +0700

    feat: validate email, password, fullname, at register controller

--- src/controllers/auth.contoller.js
+++ src/controllers/auth.contoller.js
@@ -8,8 +8,36 @@ import * as userModel from "../models/user.model.js"
 
 export function register(req, res) { 
   const { fullname, email, password } = req.body;
+  const exsisting = userModel.users.find((u) => u.email === email) 
+  if (exsisting) { 
+    res.status(409).json({
+      "message": "Email lain"
+    })
+    return
+  }
+  if (!fullname) { 
+    res.status(400).json({
+      "message": "Harus punya nama"
+    })
+    return
+  }
+  
+  if (email.indexOf('@') === -1) {
+    res.status(400).json({
+      "message": "Email tidak valid"
+    })
+    return
+  }
+  
+  if (password.length < 8) { 
+    res.status(400).json({
+      "message": "Password harus minimal 8 karakter"
+    })
+    return
+  }
+  
   const user = { 
-    id: userModel.users.length + 1,
+    id: userModel.users[userModel.users.length - 1].id + 1,
     fullname: fullname, 
     email: email, 
     password: password
