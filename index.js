044a6a8236f2d1a8b9da526c5f116aeb7f580e9e
Zacky Rafian Fawwauzy <imftcy@gmail.com>
2026-07-28 11:56:33 +0700

    feat: static file uploads

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import express from "express"
 import router from "./src/routes/index.js";
 import { urlencoded } from "express";
-
+import path from 'path';
 
 const PORT = 8080;
 const app = express()
 app.use(express.urlencoded())
+app.use('/uploads', express.static('uploads'));
+// app.all('{*all}')
 
 // app.get("/", (req, res) => { 
 //   res.json({ 
