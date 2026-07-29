26b208970cd3e03a3fa6adcd71f78f74379bdc87
Zacky Rafian Fawwauzy <imftcy@gmail.com>
2026-07-30 01:53:33 +0700

    feat: add updateProfile controller, validate fields on edit

--- src/controllers/users.contoller.js
+++ src/controllers/users.contoller.js
@@ -34,10 +34,35 @@ export function deleteUser(req, res) {
 }
 
 export function edit(req, res) { 
-  let id =  Number(req.params.id)
-  let user = usersModel.editUser(id, req.body) 
+  let id = Number(req.params.id)
+  const { fullname, email, password } = req.body; 
+  if (fullname != undefined) data.fullname = fullname
+  if (email != undefined) data.email = email
+  if (password!= undefined) data.password = password
+  
+  let user = usersModel.editUser(id, data) 
   res.json({ 
     "result": user,
   })
 }
+/**
+ * 
+ * @param {import("express").Request} req 
+ * @param {*} res 
+ */
+export function updateProfile(req, res) { 
+  let id = Number(req.params.id)
+  if (req.file?.originalname != undefined) { 
+    usersModel.editUser(id, { 
+      picture: req.file.path
+    }) 
+  }
+
+  res.status(201).json({ 
+    "id": id,
+    "success": true, 
+    "message": "upload profile success"
+  })
+}
+
 
