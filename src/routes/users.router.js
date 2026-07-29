4a2883fae81d4902fdd6bdcbc21fb4bb26ec02d6
Zacky Rafian Fawwauzy <imftcy@gmail.com>
2026-07-30 01:53:38 +0700

    feat: apply auth middleware, add upload picture route

--- src/routes/users.router.js
+++ src/routes/users.router.js
@@ -1,11 +1,16 @@
 import { Router } from "express"
 import * as usersContoller from "../controllers/users.contoller.js"
+import authMiddlewware from "../middlewares/auth.middleware.js"
+import uploadMiddleware from "../middlewares/upload.middleware.js"
+import multer from "multer"
 
 const usersRouter = Router()
 
+usersRouter.use(authMiddlewware)
 usersRouter.get('/', usersContoller.getAll)
 usersRouter.get('/:id', usersContoller.getById)
 usersRouter.delete('/:id', usersContoller.deleteUser)
 usersRouter.patch('/:id', usersContoller.edit)
+usersRouter.patch('/:id/picture', uploadMiddleware, usersContoller.updateProfile)
 
 export default usersRouter
