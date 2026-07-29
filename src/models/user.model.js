72549f58f5eba7312ad281672e4cf490af3f710a
Zacky Rafian Fawwauzy <imftcy@gmail.com>
2026-07-30 01:53:19 +0700

    feat: add sort by field at findAll

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -21,6 +21,8 @@ export const users = JSON.parse(data)
 
 console.log(users)
 
+const ALLOW_SORT = ['fullname', 'email', 'id']
+
 export function findAll(search, query, sort) {  
   const { page = 1, limit = 5 } = query || {}
   const offset = (page - 1) * limit
@@ -30,6 +32,19 @@ export function findAll(search, query, sort) {
   } else if (search?.email) { 
     result = users.filter((u) => u.email?.toLowerCase().includes(search.email.toLowerCase()))
   } 
+  if (sort && typeof sort === "object") {
+    const [sortBy, order] = Object.entries(sort)[0]
+
+    if (ALLOW_SORT.includes(sortBy)) {
+      const dir = String(order).toLowerCase() === "desc" ? -1 : 1
+      result.sort((a, b) => {
+        const A = a[sortBy]?.toString().toLowerCase() ?? ""
+        const B = b[sortBy]?.toString().toLowerCase() ?? ""
+
+        return A.localeCompare(B) * dir
+      })
+    }
+  }
   return result.slice(offset, offset + limit)
 }
 
